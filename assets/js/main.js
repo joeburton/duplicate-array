f2723d83c15e91079a379413bc3305c33a9379ea
joe burton <joeburton@gmail.com>
2015-05-06 11:29:07 +0100

    duplicate array complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
 var duplicateArray = (function () {
 
-    var copyArray = function () {
-
-        
-        return newArray;
+    var copyArray = function (oldArray) {
+        return oldArray.slice();
     };
 
     return {
-    copyArray: copyArray
+    	copyArray: copyArray
     }
 
 }());
+
+var array1 = [1, 2, 3, 4, 5];
+var array2 = duplicateArray.copyArray(array1);
+
+array1.push(6);
+
+var answer = document.getElementsByClassName('answer');
+
+answer[0].innerHTML = 'array2 is a duplicate of array1: ';
+answer[0].innerHTML += (array1 !== array2) ? '<b>true</b>' : '<b>false</b>';
+
+console.log('array1: ', array1, 'array2: ', array2);
